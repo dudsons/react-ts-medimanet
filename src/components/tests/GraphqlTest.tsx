30d43fba4acee67e74fe538134447cb0b5e07b76
rydzewskima <postgres>
2021-01-11 10:09:09 +0100

    Displaying result of graphql query

--- src/components/tests/GraphqlTest.tsx
+++ src/components/tests/GraphqlTest.tsx
@@ -4,13 +4,40 @@ import '../../css/Room.css'
 import {
     ApolloClient,
     gql,
-    useMutation,
     NormalizedCacheObject
 } from '@apollo/client';
 import {cache} from "../../cash";
+import './GraphqlHookTest';
 
+interface Movie {
+    id:number,
+    author:string,
+    url:string
+}
 
-export default class GraphqlTest extends Component <any, any> {
+interface IProps {
+
+}
+
+interface Movie {
+    id:number,
+    author:string,
+    url:string
+}
+
+interface Movies {
+    movies:Movie[]
+}
+
+export default class GraphqlTest extends Component <IProps, Movies> {
+
+    constructor(props:IProps){
+        super(props);
+        this.state = {
+            movies: []
+        }
+
+    }
 
     client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
         cache,
@@ -30,18 +57,27 @@ export default class GraphqlTest extends Component <any, any> {
                 }
             `
         })
-        .then(result => console.log(result));
+        .then(result => { console.log(result);
+            this.setState({
+                movies:result.data.getMovies
+            })
+
+        });
     }
 
+
+
     render() {
-        useQu
-        return (
+            return (
             <div>
                 <div>
                     <p>Graphql tests buttons</p>
                     <button onClick={()=>this.getMoviesFromServer()}>Graphql Get movies</button>
-                    <p ></p>
                 </div>
+                <p>Message from server </p>
+                <div>{this.state.movies.map(function (d,idx){
+                    return (<li key={idx}>{d.id},{d.author}</li>)
+                })}</div>
             </div>
         );
     }
