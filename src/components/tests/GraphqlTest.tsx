9a657ea93a5ec5fc80496101115ee82ac291a8aa
rydzewskima <postgres>
2021-01-11 14:52:54 +0100

    Added function to displaying current Movies in db

--- src/components/tests/GraphqlTest.tsx
+++ src/components/tests/GraphqlTest.tsx
@@ -4,6 +4,7 @@ import '../../css/Room.css'
 import {
     ApolloClient,
     gql,
+    useMutation,
     NormalizedCacheObject
 } from '@apollo/client';
 import {cache} from "../../cash";
@@ -29,6 +30,9 @@ interface Movies {
     movies:Movie[]
 }
 
+
+
+
 export default class GraphqlTest extends Component <IProps, Movies> {
 
     constructor(props:IProps){
@@ -36,8 +40,15 @@ export default class GraphqlTest extends Component <IProps, Movies> {
         this.state = {
             movies: []
         }
-
     }
+
+     ADD_MOVIES = gql`
+        mutation AddMovie($author: String!, $url:String){
+            addMovie(author: $author, url: $url){
+                author,
+                url
+            }
+        }`;
 
     client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
         cache,
@@ -50,9 +61,10 @@ export default class GraphqlTest extends Component <IProps, Movies> {
         .query({
             query: gql`
                 query TestQuery {
-                    getMovies(limit:10){
+                    getMovies(limit:100){
                         id,
-                        author
+                        author,
+                        url,
                     }
                 }
             `
@@ -65,8 +77,44 @@ export default class GraphqlTest extends Component <IProps, Movies> {
         });
     }
 
+    author2:string = "xxxx";
+    url2:string = "yyyyy";
 
+    addMovieToDb2(e:any){
+        e.preventDefault();
+        // const author2:string = (document.getElementById('author')as HTMLInputElement).value;
+        // const url2:string = (document.getElementById('url')as HTMLInputElement).value;
+        this.client.mutate({
+            mutation: this.ADD_MOVIES,
+            variables: {author:this.author2, url:this.url2},
+        })
+            .then((response) => console.log(response.data))
+            .catch((err) => console.error(err));
 
+    }
+    // addMovieToDb() {
+    //     // e.preventDefault();
+    //     if(document.getElementById('author') != null ||
+    //         document.getElementById('url') != null) {
+    //         const author2:string = (document.getElementById('author')as HTMLInputElement).value;
+    //         const url2:string = (document.getElementById('url')as HTMLInputElement).value;
+    //         console.log(`author to save: `+ author2 + ' url: ' + url2);
+    //     }
+    //
+    //     this.client
+    //     .mutate({
+    //     mutation: gql`
+    //             mutation TestQuery2 {
+    //                 addMovie(author:author2,url: url2){
+    //                     id,
+    //                     author
+    //                 }
+    //             }
+    //         `
+    //     })
+    //
+    //     .then(result => console.log(result));
+    // }
     render() {
             return (
             <div>
@@ -74,12 +122,19 @@ export default class GraphqlTest extends Component <IProps, Movies> {
                     <p>Graphql tests buttons</p>
                     <button onClick={()=>this.getMoviesFromServer()}>Graphql Get movies</button>
                 </div>
-                <p>Message from server </p>
+                <form>
+                    <label>Add author</label><input type="text" id="author"/>
+                    <label>Add url</label><input type="text" id="url"/>
+                    <button onClick={()=>this.addMovieToDb2}>Add movie to db</button>
+                </form>
+                <p>Films in DB:</p>
                 <div>{this.state.movies.map(function (d,idx){
-                    return (<li key={idx}>{d.id},{d.author}</li>)
+                    return (<li key={idx}>{d.id},{d.author},{d.url}</li>)
                 })}</div>
             </div>
         );
     }
+
+
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import {
-    ApolloClient,
-    gql,
-    useMutation,
-    NormalizedCacheObject
-} from '@apollo/client';
+import {ApolloClient, NormalizedCacheObject, ApolloProvider} from '@apollo/client';
 import {cache} from "./cash";
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
@@ -17,51 +11,12 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     uri: 'http://localhost:8080/graphql'
 });
 
-// ...ApolloClient instantiated here...
-
-client
-.mutate({
-    mutation: gql`
-        mutation TestQuery {
-            addMovie(author:"Mrco",url: "io.pl"){
-                id,
-                author
-            }
-        }
-    `
-})
-
-.then(result => console.log(result));
-
-
-client
-.query({
-    query: gql`
-        query TestQuery {
-            getMovies(limit:10){
-                id,
-                author
-            }
-        }
-    `
-})
-
-.then(result => console.log(result));
-
-const randomQuotes: string[] = [
-    "Before you judge a man, walk a mile in his shoes. After that who cares?... He’s a mile away and you’ve got his shoes!",
-    "Better to remain silent and be thought a fool than to speak out and remove all doubt.",
-    "The best thing about the future is that it comes one day at a time.",
-    "The only mystery in life is why the kamikaze pilots wore helmets.",
-    "Light travels faster than sound. This is why some people appear bright until you hear them speak.",
-    "The difference between stupidity and genius is that genius has its limits"
-]
-
 ReactDOM.render(
     <React.StrictMode>
+        <ApolloProvider client={client}>
         <App/>
         {/*<QuoteApp quotes={randomQuotes}/>*/}
-
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
