d489883e476a5cbf26cacc7fe1c1bdbdfa21431e
rydzewskima <postgres>
2021-01-13 09:39:32 +0100

    Displaying movie films list using hooks

--- src/components/tests/MovieFromDbHooksGql.tsx
+++ src/components/tests/MovieFromDbHooksGql.tsx
@@ -24,13 +24,16 @@ function MovieFromDbHooksGql () {
 
     if(loading) return <div>Loading...</div>;
     if(error) return <div>Error can not get movies</div>
+
     return (
         <div>
             <div>
                 <p>Graphql tests buttons</p>
             </div>
             <p>Films in DB:</p>
-            <div>{data?.getMovies[0].author} </div>
+            <div>{data?.getMovies.map((data,idx)=>{
+                return(<li key={idx} >{data.id},{data.author},{data.url}</li>)
+            })} </div>
         </div>
     );
 }
@@ -39,7 +42,8 @@ function MovieFromDbHooksGql () {
 const GET_MOVIES = gql`
     query GetMovies($limit: Int!){
         getMovies(limit: $limit){
-            author
+            id,
+            author,
             url
         }
     }`;
