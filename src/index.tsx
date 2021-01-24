22628c0f7d68163299cbc1d07d2abb357f3e6964
MarcoRy <rydzewski.marek86@gmail.com>
2021-01-24 15:28:08 +0100

    Created methods for handling JTW in graphql query

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ApolloClient, NormalizedCacheObject, ApolloProvider, InMemoryCache} from '@apollo/client';
+import {ApolloClient, createHttpLink, NormalizedCacheObject, ApolloProvider, InMemoryCache} from '@apollo/client';
+import {setContext} from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+        uri: "http://localhost:8080/graphql"
+    }
+);
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('token');
+    // const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJlazEiLCJpYXQiOjE2MTE0OTM0MjksImV4cCI6MTYxMTU3OTgyOX0.WmKK-fnt76OTN7nW8eDjcivd2Gni_A4XGbiRFywWr_uGqcGillZGq6gVKaXnMJcJ7DaHht-nbrDLqqbiWyvSoA"
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache({
         resultCaching: false,
-        addTypename:false
+        addTypename: false
     }),
-    uri: 'http://localhost:8080/graphql'
+    // uri: 'http://localhost:8080/graphql'
 });
 
 ReactDOM.render(
