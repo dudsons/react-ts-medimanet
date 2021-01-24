3c113a7e302aab2d1521a7db4869be243c012103
MarcoRy <rydzewski.marek86@gmail.com>
2021-01-24 20:26:33 +0100

    Home page component

---
+++ src/security/components/Home.tsx
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react';
+import UserService from '../services/user.service';
+
+const Home = () => {
+
+    const [content, setContent] = useState<any>();
+
+
+    useEffect(()=>{
+        UserService.getPublicContent().then(
+            (response) => {
+                setContent(response!);
+            },
+            (error) =>{
+                const errMessage = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+                setContent(errMessage);
+            }
+        )
+    },[]);
+
+    return (
+        <div className="container">
+            <header className="jumbotron">
+                <h3>{content}</h3>
+            </header>
+        </div>
+    )
+};
+
+export default Home;
