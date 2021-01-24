d270208e5568f628c1776cb3389cf80ded429e32
MarcoRy <rydzewski.marek86@gmail.com>
2021-01-24 20:23:57 +0100

    Authorization service: -register, login, logout, getCurrentUser

---
+++ src/security/services/auth.service.tsx
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const URL_API = 'http://localhost:8080/api/auth/';
+
+const register = (username: string, email: string, password: string) => {
+    return axios.post(URL_API + "signup", {
+        username, email, password
+    })
+};
+
+const login = (username: string, password: string) => {
+    return axios.post('signup', {
+        username, password
+    }).then((response)=>{
+        if(response.data.token){
+            localStorage.setItem("user",response.data);
+        }
+        return response.data;
+    })
+};
+
+const logout = () =>{
+    localStorage.removeItem("user")
+};
+
+const getCurrentUser =() =>{
+    return JSON.parse(localStorage.getItem("user")!);
+};
+
+export default {
+    logout,
+    login,
+    register,
+    getCurrentUser
+}
