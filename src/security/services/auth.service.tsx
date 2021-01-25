92ccbcf987265598470891cd9ef803a7f0dee33f
rydzewskima <postgres>
2021-01-25 14:49:17 +0100

    Fixed small mistake

--- src/security/services/auth.service.tsx
+++ src/security/services/auth.service.tsx
@@ -2,17 +2,18 @@ import axios from 'axios'
 
 const URL_API = 'http://localhost:9000/api/auth/';
 
-const register = (username: string, email: string, password: string) => {
+const register = (username: string, password: string, email: string) => {
     return axios.post(URL_API + "signup", {
         username, email, password
     })
 };
 
 const login = (username: string, password: string) => {
-    return axios.post('signup', {
+    return axios.post(URL_API + 'signin', {
         username, password
     }).then((response)=>{
         if(response.data.token){
+
             localStorage.setItem("user",response.data);
         }
         return response.data;
