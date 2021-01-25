79c7d99a5fabfc0d590bb457dc6baea8bd8b16e1
rydzewskima <postgres>
2021-01-25 13:10:14 +0100

    Changed password min length to 3

--- src/security/components/Register.tsx
+++ src/security/components/Register.tsx
@@ -35,10 +35,10 @@ const validUsername = (username: string) => {
     }
 };
 const validPassword = (password: string) => {
-    if (password.length < 6 || password.length > 40) {
+    if (password.length < 3 || password.length > 40) {
         return (
             <div className='alert alert-danger' role='alert'>
-                Password should be between 6 and 40 characters!
+                Password should be between 3 and 40 characters!
             </div>
         )
     }
